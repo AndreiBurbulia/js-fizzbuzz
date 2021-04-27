419380a83f4eb46566b6e942971557ac5b84d6a6
AndreiBurbulia <Andreiburbulia@hotmail.it>
2021-04-27 15:37:36 +0200

    Aggiunta foglio di stile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,48 @@
+
+
 // stampare i numeri da 1 a 100
-var numero = Number(Math.ceil(Math.random() * 99));
-console.log(numero);
+var numero = Number(Math.ceil(Math.random() * 100));
+document.getElementById("numero").innerHTML = numero;
+
 
 //verificare se i numeri sono divisibili per 3 e per 5 
 
+//Svolto con switch
+switch (true) {
+
+    //se e divisibile sia per 3 che per 5 stampo FizzBuzz
+    case numero % 3 == 0 && numero % 5 == 0:
+        document.getElementById("divisibile_5_3").innerHTML = "FizzBuzz";
+        break;
+
+    //se e divisibile per 3 allora stampo Fizz
+    case numero % 3 == 0:
+        document.getElementById("divisibile_3").innerHTML = "Fizz";
+        break;
+
+    //se e divisibile per 5 stampo Buzz
+    case numero % 5 == 0:
+        document.getElementById("divisibile_5").innerHTML = "Buzz";
+        break;
+}
+
+
+//svolto con IF
+
+/*
+//se e divisibile sia per 3 che per 5 stampo FizzBuzz
+if (numero % 3 == 0 && numero % 5 == 0) {
+    document.getElementById("divisibile_5_3").innerHTML = "FizzBuzz";
+}
+
 //se e divisibile per 3 allora stampo Fizz
+else if (numero % 3 == 0) {
+    document.getElementById("divisibile_3").innerHTML = "Fizz";
+}
 
 //se e divisibile per 5 stampo Buzz
-
-//se e divisibile sia per 3 che per 5 stampo FizzBuzz
-
-
-var prova3 = numero % 3;
-console.log(prova3);
-var prova5 = numero % 5;
-console.log(prova5);
-
-
-if (prova3 == 0 && prova5 == 0) {
-    document.getElementById("risultato").innerHTML = "FizzBuzz";
-} else if (prova3 == 0) {
-    document.getElementById("risultato").innerHTML = "Fizz";
-} else if (prova5 == 0) {
-    document.getElementById("risultato").innerHTML = "Buzz";
-} else {
-    document.getElementById("risultato").innerHTML = numero;
-
+else if (numero % 5 == 0) {
+    document.getElementById("divisibile_5").innerHTML = "Buzz";
 }
+
+*/
